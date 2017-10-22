030c816cbf9e9ec9f65bdb0c3aa47bb947fe861e
Wesley <wesleydmuniz@gmail.com>
2017-10-21 23:33:14 -0200

    Delegating xml operations to xmlservice

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
 # coding=utf-8
 import sys, socket, select
 import time
-from lxml import etree
-from cStringIO import StringIO
+from xmlservice import XmlService
 
 class Student:
 
-    STATIC_PATH = 'static/%s'
     SUBMIT_STATUS_MESSAGES = ['sucesso', 'XML inválido', 'XML mal-formado', 'Erro Interno']
     CONSULT_STATUS_MESSAGES = ['Candidato não encontrado', 'Em processamento',
         'Candidato Aprovado e Selecionado',
@@ -20,33 +18,6 @@ class Student:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((host, port))
 
-    def _getXml(self, xml_filename):
-        parsed_xml = ''
-        filepath = 'static/%s' % xml_filename
-        with open(filepath) as xml:
-            parsed_xml = xml.read()
-        return parsed_xml
-
-    def _convertInvalidXml(self, xml_filename):
-
-        valid_xml = None
-        xml_filepath = self.STATIC_PATH % xml_filename
-        with open(xml_filepath):
-            dom = etree.parse(xml_filepath)
-
-        xsl_filename = 'transform_boletim.xsl'
-        xsl_filepath = self.STATIC_PATH % xsl_filename
-        with open(xsl_filepath):
-            xslt = etree.parse(xsl_filepath)
-
-        try:
-            transform = etree.XSLT(xslt)
-            newdom = transform(dom)
-            valid_xml = etree.tostring(newdom, pretty_print=True)
-        except:
-            print "deu ruim"
-        return valid_xml
-
     def __prepareMessage(self, method_name, parameter_name):
         message = '<?xml version="1.0" encoding="utf-8" ?>\n \
             <request xmlns="https://www.w3schools.com">\n \
@@ -59,8 +30,8 @@ class Student:
 
     def submit(self, xml):
 
-        if self.__checkXmlStatus(xml, 'boletim.xsd'):
-            xml = self._convertInvalidXml('boletim.xml')
+        if XmlService.checkXmlStatus(xml, 'boletim.xsd'):
+            xml = XmlService.convertInvalidXml('boletim.xml')
 
         submit_message =  self.__prepareMessage('submeter', xml)
         self.s.send(submit_message)
@@ -71,22 +42,7 @@ class Student:
 
         xsd_filename = 'status_submeter.xsd'
 
-        return self.__checkXmlStatus(xml, xsd_filename)
-
-    def retrieve_consulta_status_value(self, xml):
-        doc = etree.parse(StringIO(xml))
-        request_elem = doc.getiterator().__next__()
-        parameter_elem = request_elem.iterchildren().__next__().getnext()
-        consultar_status_elem = parameter_elem.iterchildren().__next__()
-        status_text = consultar_status_elem.text
-
-        status_value = 0
-        for i in status_text:
-            if i.isdigit():
-                status_value = int(i)
-                break
-        return status_value
-
+        return XmlService.checkXmlStatus(xml, xsd_filename)
 
     def consult_status(self, cpf):
 
@@ -101,46 +57,13 @@ class Student:
 
         xsd_filename = 'consultar_status.xsd'
 
-        return self.retrieve_consulta_status_value(xml)
+        return XmlService.retrieve_consulta_status_value(xml)
 
-    def __checkXmlStatus(self, xml, xsd_filename):
-        xsd = ''
-        status = None
-        filepath = self.STATIC_PATH % xsd_filename
-        with open(filepath) as xsd_file:
-            xsd = xsd_file.read()
-        xmlschema_doc = etree.parse(StringIO(xsd))
-        xmlschema = etree.XMLSchema(xmlschema_doc)
-
-        # Parse XML
-        try:
-            doc = etree.parse(StringIO(xml))
-        except IOError:
-            status = 3
-        except etree.XMLSyntaxError:
-            print "Deu ruim"
-            status = 1
-
-        if status:
-            return status
-
-        # Validate XML
-        try:
-            xmlschema.assertValid(doc)
-            status = 0
-
-        except etree.DocumentInvalid as err:
-            status = 2
-
-        except:
-            status = 3
-
-        return status
 
 student = Student()
 student._connect()
 
-xml = student._getXml('boletim.xml')
+xml = XmlService.getXml('boletim.xml')
 print "Submetendo arquivo para o servidor"
 print student.SUBMIT_STATUS_MESSAGES[student.submit(xml)]
 
---
+++ xmlservice.py
@@ -0,0 +1,88 @@
+# coding=utf-8
+from lxml import etree
+from cStringIO import StringIO
+
+class XmlService:
+
+    STATIC_PATH = 'static/%s'
+
+    @staticmethod
+    def getXml(xml_filename):
+        parsed_xml = ''
+        filepath = XmlService.STATIC_PATH % xml_filename
+        with open(filepath) as xml:
+            parsed_xml = xml.read()
+        return parsed_xml
+
+    @staticmethod
+    def convertInvalidXml(xml_filename):
+
+        valid_xml = None
+        xml_filepath = XmlService.STATIC_PATH % xml_filename
+        with open(xml_filepath):
+            dom = etree.parse(xml_filepath)
+
+        xsl_filename = 'transform_boletim.xsl'
+        xsl_filepath = XmlService.STATIC_PATH % xsl_filename
+        with open(xsl_filepath):
+            xslt = etree.parse(xsl_filepath)
+
+        try:
+            transform = etree.XSLT(xslt)
+            newdom = transform(dom)
+            valid_xml = etree.tostring(newdom, pretty_print=True)
+        except:
+            print "Erro ao converter XML"
+        return valid_xml
+
+    @staticmethod
+    def checkXmlStatus(xml, xsd_filename):
+        xsd = ''
+        status = None
+        filepath = XmlService.STATIC_PATH % xsd_filename
+        with open(filepath) as xsd_file:
+            xsd = xsd_file.read()
+        xmlschema_doc = etree.parse(StringIO(xsd))
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+
+        # Parse XML
+        try:
+            doc = etree.parse(StringIO(xml))
+        except IOError:
+            status = 3
+        except etree.XMLSyntaxError:
+            status = 1
+
+        if status:
+            return status
+
+        # Validate XML
+        try:
+            xmlschema.assertValid(doc)
+            status = 0
+
+        except etree.DocumentInvalid as err:
+            status = 2
+
+        except:
+            status = 3
+
+        return status
+
+    @staticmethod
+    def retrieve_consulta_status_value(xml):
+        status_value = 0
+        try:
+            doc = etree.parse(StringIO(xml))
+            request_elem = doc.getiterator().__next__()
+            parameter_elem = request_elem.iterchildren().__next__().getnext()
+            consultar_status_elem = parameter_elem.iterchildren().__next__()
+            status_text = consultar_status_elem.text
+        except:
+            status_text = ''
+
+        for i in status_text:
+            if i.isdigit():
+                status_value = int(i)
+                break
+        return status_value
